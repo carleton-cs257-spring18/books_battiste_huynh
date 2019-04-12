8758be4a8c52c95cd3e2adcc61afcbbe724b5aec
AlexBattiste <battistea@carleton.edu>
2019-04-12 14:32:59 -0500

    Added no parameters test for books method

--- Books/books_phase2/booksdatasourcetest.py
+++ Books/books_phase2/booksdatasourcetest.py
@@ -9,7 +9,7 @@ class BooksDataSourceTest(unittest.TestCase):
         pass
     
     '''
-        Unit test for book
+        Unit tests for book
     '''
     def test_book_zero(self):
         self.assertEqual("All Clear", self.booksDataSource.book(0))
@@ -30,11 +30,11 @@ class BooksDataSourceTest(unittest.TestCase):
         self.assertRaises(ValueError, self.booksDataSource.book, 100)
         
     '''
-        Unit test for books
+        Unit tests for books
     '''
 
     def test_books_no_parameters(self):
-        pass
+        self.assertEqual([{0:'All Clear'}, {1:'And Then There Were None'}, {2:'Beloved'}, {3:'Blackout'}, {4:'Elmer Gantry'}, {5:'Emma'}, {6:'Good Omens'}, {7:'Jane Eyre'}, {8:'Leave it to Psmith'}, {9:'Love in the Time of Cholera'}, {10:'Main Street'}, {11:"Midnight's Children"}, {12:'Mirror Dance'}, {13:'Moby Dick'}, {14:'Murder on the Orient Express'}, {15:'Neverwhere'}, {16:'Omoo'}, {17:'One Hundred Years of Solitude'}, {18:'Pride and Prejudice'}, {19:'Right Ho, Jeeves'}, {20:'Sense and Sensibility'}, {21:'Shards of Honor'}, {22:'Sula'}, {23:'The Code of the Woosters'}, {24:'The Satanic Verses'}, {25:'The Tenant of Wildfell Hall'}, {26:'Thief of Time'}, {27:'To Say Nothing of the Dog'}, {28:'Villette'}, {29:'Wuthering Heights'}, {30:'1Q84'}, {31:'A Wild Sheep Chase'}, {32:'Hard-Boiled Wonderland and the End of the World'}, {33:'My Antonia'}, {34:'O Pioneers!'}, {35:'The Power'}, {36:'Rebecca'}, {37:'The Fifth Season'}, {38:'The Obelisk Gate'}, {39:'The Stone Sky'}, {40:'Three Men in a Boat (to Say Nothing of the Dog)'}, {41:'Middlemarch'}, {42:'Silas Marner'}, {43:'Bleak House'}, {44:'Great Expectations'}, {45:'Tinker, Tailor, Soldier, Spy'}, {46:'The Spy Who Came in From the Cold'}], self.booksDataSource.books())
     
     def test_books_valid_author_ID(self):
         self.assertEqual(self.booksDataSource.books(author_id=1), ["And Then There Were None", "Murder on the Orient Express"])
@@ -67,7 +67,7 @@ class BooksDataSourceTest(unittest.TestCase):
         self.assertNotEqual(self.booksDataSource.books(start_year = 1990), [])
         
     def test_books_end_year(self):
-        self.assertEqual(self.booksDataSource.books(start_year = 1830), ['Emma', 'Pride and Prejudice', 'Sense and Sensibility'])
+        self.assertEqual(self.booksDataSource.books(end_year = 1830), ['Emma', 'Pride and Prejudice', 'Sense and Sensibility'])
         
     def test_books_end_year_wrong_books(self):
         self.assertNotEqual(self.booksDataSource.books(start_year = 2010), ['Emma', 'Pride and Prejudice', 'Sense and Sensibility'])
@@ -80,6 +80,12 @@ class BooksDataSourceTest(unittest.TestCase):
         
     def test_books_sort_by_year_wrong_order(self):
         self.assertNotEqual(self.booksDataSource.books(start_year = 2015, sort_by = 'year'), ['The Power', 'The Fifth Season', 'The Obelisk Gate', 'The Stone Sky'])
+
+    def test_books_author_ID_and_start_year(self):
+        self.assertEqual(self.booksDataSource.books(author_id=1, start_year=1939), ["And Then There Were None"])
+
+    def test_books_search_text_and_end_year(self):
+        self.assertEqual(self.booksDataSource.books(end_year=1830, search_text='and'), ['Pride and Prejudice', 'Sense and Sensibility'])
         
     '''
         Unit test for author
